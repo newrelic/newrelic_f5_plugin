14dc977085d7c54d98b2273ac986d5b69ef59ca7
Jonathan Thurman <jthurman@newrelic.com>
2013-10-11 16:38:40 -0700

    Fix failing test with updated newrelic_agent version

--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -13,11 +13,10 @@ newrelic:
 agents:
   f5:
     -
-      name:           'My F5 LTM'
       hostname:       'my-f5'
       port:           161
       snmp_community: 'public'
-      EOF
+EOF
       NewRelic::Plugin::Setup.install_agent :f5, NewRelic::F5Plugin
     end
 
@@ -40,36 +39,8 @@ agents:
       end
 
       should "have one configured agent" do
-        assert_equal "My F5 LTM", @agent.name
+        assert_equal "my-f5", @agent.hostname
       end
-
-      #context "test db" do
-      #  setup do
-      #    # Setup a test database
-      #    @client = Mysql2::Client.new :host => 'localhost', :username => 'root'
-      #    @client.query "create database nr_mysql_plugin" rescue nil
-      #    @client.query "create table nr_mysql_plugin.example (id int)"
-      #    @client.query "create table nr_mysql_plugin.ignored (id int)"
-      #    @client.query "insert into nr_mysql_plugin.example values (1),(2),(3),(4),(5)"
-      #  end
-      #  teardown do
-      #    @client.query "drop database nr_mysql_plugin" rescue nil
-      #  end
-      #  should "get table stats" do
-      #    schemas = @agent.mysql_table_stats
-      #    assert_equal 1, schemas.size
-      #    tables = schemas['nr_mysql_plugin']
-      #    assert_equal 1, tables.size
-      #    rec = tables.first
-      #    assert_equal "example", rec.name
-      #    assert_equal 5, rec.rows
-      #  end
-      #  should "poll" do
-      #    # This stubs out the remote connection--we're not testing the plugin agent here
-      #    NewRelic::Plugin::DataCollector.any_instance.stubs :process
-      #    @agent.run 60 # seconds
-      #  end
-      #end
 
     end
 
